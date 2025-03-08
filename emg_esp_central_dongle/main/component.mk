2e8c1ca5c23bbe55f6934f9bff0caf249abe70dd
Bruno <brunorchaves1@gmail.com>
2025-03-07 23:53:59 -0300

    criação do projeto correto do esp32 para o dongle, ja scaneia os dispositivos

--- emg_esp_central_dongle/main/component.mk
+++
@@ -1,5 +0,0 @@
-#
-# "main" pseudo-component makefile.
-#
-# (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
-ENT_EMBED_TXTFILES := certs/drive_google_com.pem
