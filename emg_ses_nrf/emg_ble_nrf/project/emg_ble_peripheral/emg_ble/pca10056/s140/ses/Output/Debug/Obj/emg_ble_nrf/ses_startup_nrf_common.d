7822226141230237597264bc4a775704da72541f
Bruno <brunorchaves1@gmail.com>
2025-04-06 19:24:45 -0300

    projeto do nrf no ses

---
+++ emg_ses_nrf/emg_ble_nrf/project/emg_ble_peripheral/emg_ble/pca10056/s140/ses/Output/Debug/Obj/emg_ble_nrf/ses_startup_nrf_common.d
@@ -0,0 +1,2 @@
+Output/Debug/Obj/emg_ble_nrf/ses_startup_nrf_common.o: \
+ C:\Users\bruno.ribeiro\OneDrive\ -\ Universidade\ Federal\ de\ Minas\ Gerais\Área\ de\ Trabalho\nRF5_SDK_17.1.0_ddde560\modules\nrfx\mdk\ses_startup_nrf_common.s
---
+++ emg_ses_nrf/emg_ble_nrf/project/emg_ble_peripheral/emg_ble/pca10056/s140/ses/Output/Debug/Obj/emg_ble_nrf/thumb_crt0.d
@@ -0,0 +1,2 @@
+Output/Debug/Obj/emg_ble_nrf/thumb_crt0.o: \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ 8.22a/source/thumb_crt0.s
